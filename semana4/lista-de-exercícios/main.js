81cc659180af4facd19ee932da9595284653c5f8
Hugo Lacerda <hugolacerdar@gmail.com>
2020-04-11 20:32:14 -0300

    b03e04

--- semana4/lista-de-exercícios/main.js
+++ semana4/lista-de-exercícios/main.js
@@ -206,32 +206,66 @@
 // }
 
 // 3.
-const array = [1, 2, 3, 4, 5, 6];
+// const array = [1, 2, 3, 4, 5, 6];
+// // a.
+// function arrayPor2(array){
+//     valoresDobrados = array.map((num)=>{
+//         return num * 2;
+//     })
+
+//     return valoresDobrados;
+// }
+// // b.
+// function arrayPor3Strigfy(array){
+//     valoresTriplicadosStr = array.map((num)=>{
+//         return (num * 3).toString();
+//     })
+
+//     return valoresTriplicadosStr;
+// }
+// // c.
+// function arrayParouImpar(array){
+//     parOuImpar = array.map((num)=>{
+//         if(num % 2 === 0){
+//             return `${num} é par`;
+//         }else{
+//             return `${num} é impar`;
+//         }
+//     })
+
+//     return parOuImpar;
+// }
+
+// 4.
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
 // a.
-function arrayPor2(array){
-    valoresDobrados = array.map((num)=>{
-        return num * 2;
+function permitidas(array){
+    let pessoasPermitidas = array.filter(pessoa =>{
+        if(pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60){
+            return true;
+        }
+        
+        return false;
     })
 
-    return valoresDobrados;
+    return pessoasPermitidas;
 }
 // b.
-function arrayPor3Strigfy(array){
-    valoresTriplicadosStr = array.map((num)=>{
-        return (num * 3).toString();
-    })
-
-    return valoresTriplicadosStr;
-}
-// c.
-function arrayParouImpar(array){
-    parOuImpar = array.map((num)=>{
-        if(num % 2 === 0){
-            return `${num} é par`;
-        }else{
-            return `${num} é impar`;
+function naoPermitidas(array){
+    let pessoasProibidas = array.filter(pessoa =>{
+        if(pessoa.altura < 1.5 || (pessoa.idade <= 14 || pessoa.idade >= 60)){
+            return true;
         }
+        
+        return false;
     })
 
-    return parOuImpar;
+    return pessoasProibidas;
 }
