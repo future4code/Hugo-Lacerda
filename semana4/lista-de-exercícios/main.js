22c6b67c160f9a0f1824da0e553e2cf99a0cd1b0
Hugo Lacerda <hugolacerdar@gmail.com>
2020-04-11 11:59:06 -0300

    b01e01

---
+++ semana4/lista-de-exercícios/main.js
@@ -0,0 +1,3 @@
+// Bloco 1, parte 1
+// 1. O trecho de código cria uma função que recebe como parâmetro um valor em dólar. Dentro desta função, é pedido um input ao usuário (convertido automaticamente para Number) referente a cotação do dolar para reais e, por fim, a função retorna o valor do argumento dado como parâmetro multiplicado pela entrada do usuário, equivalendo ao valor em reais. A função é chamada abaixo na atribuição da variável declarada meuDinheiro, na qual é dado 100 como parâmetro. O valor será impresso no console após o cálculo, que dependerá com o input do usuário.
+
