f11ef2b81d3fb639bf35b5161b5f342f3d2e2034
Hugo Lacerda <hugolacerdar@gmail.com>
2020-07-12 22:02:11 -0300

    Ok

--- semana16/todo-list-backend/src/index.ts
+++ semana16/todo-list-backend/src/index.ts
@@ -2,7 +2,7 @@ import knex from "knex";
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import { AddressInfo } from "net";
-
+import moment from "moment";
 /**************************************************************/
 
 dotenv.config();
@@ -96,7 +96,7 @@ app.get(
 
 // 3
 
-const editUser = async (id: string, newName: string, newUsername: string): Promise<any> => {
+const editUser = async (id: string, newName?: string, newUsername?: string): Promise<any> => {
   const user = await connection("User").update({
     name: newName,
     username: newUsername
@@ -107,6 +107,11 @@ app.put(
   "/user/edit/:id",
   async (req: Request, res: Response): Promise<void> => {
     try {
+
+      if(req.body.name === "" || req.body.username === "" || !req.params.id){
+        throw new Error("Failed: Review your parameters")
+      }
+      
       await editUser(req.params.id, req.body.name, req.body.username);
 
       res.status(200).send({
@@ -121,3 +126,75 @@ app.put(
 );
 
 // 4
+
+const createTask = async (
+  title: string, 
+  description: string, 
+  deadline: moment.Moment,
+  creator_id: string
+  ): Promise<any> => {
+    await connection
+    .insert({
+      id: Date.now().toString(),
+      title: title,
+      description: description,
+      deadline: deadline.format("YYYY-MM-DD"),
+      creator_id: creator_id
+    })
+    .into("Task")
+  }
+
+
+  app.put("/task", async(req: Request, res: Response): Promise<void> => {
+    try{
+      if(!req.body.title || !req.body.description || !req.body.deadline || !req.body.creator_id){
+        throw new Error('Failed: Review your parameters')
+      }
+
+      const date = moment(req.body.deadline, "DD/MM/YYYY");
+      await createTask(req.body.title, req.body.description, date, req.body.creator_id);
+
+      res.status(200).send({
+        message: "Success!",
+      });
+    }catch(err){
+      res.status(400).send({
+        meassage: err.message,
+      })
+    }
+  });
+
+  // 5
+
+  const getTaskById = async (id: string): Promise<any> => {
+    const task = await connection.raw(`
+      SELECT t.id as taskId, 
+      t.title,
+      t.description,
+      t.deadline,
+      t.status,
+      t.creator_id,
+      u.username as creator_username
+      FROM Task AS t
+      JOIN User AS u
+      ON t.creator_id = u.id
+      WHERE t.id = ${id}
+    `);
+  
+    return task[0][0];
+  };
+  
+  app.get(
+    "/task/:id",
+    async (req: Request, res: Response): Promise<void> => {
+      try {
+        const task = await getTaskById(req.params.id);
+        task.deadline = moment(task.deadline, "YYYY-MM-DD").format("DD/MM/YYYY")
+        res.status(200).send(task);
+      } catch (err) {
+        res.status(400).send({
+          message: err.message,
+        });
+      }
+    }
+  );
